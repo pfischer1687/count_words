a3ba1b31c1b72712c7e799004c1e692dedc6b49d
pfischer1687 <pfisch789@outlook.com>
2023-06-25 23:00:28 -0700

    Adds unit test template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use count_words::add;
+
 fn main() {
-    println!("Hello, world!");
+    let left: usize = 2;
+    let right: usize = 2;
+    let result: usize = add(left, right);
+    println!("result = {result}");
 }
