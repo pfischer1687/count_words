2e097512e9d0a926045aea6d3c3f5b497a78527f
pfischer1687 <pfisch789@outlook.com>
2023-06-28 21:19:53 -0700

    Read in text file name as command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+// cargo run txt/opticks.txt
+
 use count_words::get_most_common_words;
-use std::{fs::File, io::BufReader, io::prelude::*};
+use std::{fs::File, io::BufReader, io::prelude::*, env};
 
 fn main() -> std::io::Result<()> {
+    let text_file: String = match env::args().nth(1) {
+        Some(text_file) => text_file,
+        None => { panic!("Error: text file name command line argument required"); },
+    };
+
     let mut contents: String = String::new();
     let n: usize = 10;
     let mut result: Vec<(String, i32)> = Vec::new();
 
-    let file: File = File::open("txt/opticks.txt")?;
+    let file: File = File::open(text_file)?;
     let mut buf_reader: BufReader<File> = BufReader::new(file);
     buf_reader.read_to_string(&mut contents)?;
 
